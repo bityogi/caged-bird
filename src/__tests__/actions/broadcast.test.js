92dcfc2048d9d4885b713ee55ce3ec4d9db6c4c5
bityogi <bityogi@tutanota.com>
2018-03-12 17:12:27 -0400

    tests -- broadcast actions

--- src/__tests__/actions/broadcast.test.js
+++ src/__tests__/actions/broadcast.test.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import { mount } from 'enzyme';
+import { Thunk } from 'redux-testkit';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import MockAdapter from 'axios-mock-adapter';
 
-import Header from 'components/header';
+import { client } from 'util/axios';
+import * as actions from 'actions';
+import * as types from 'actions/types';
 
-describe('Broadcast', () => {
-  it('placehoder test', () => {
-    expect(2 + 2).toBe(4);
-  })
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const mock = new MockAdapter(client);
+
+describe('Broadcast actions', () => {
+  afterEach(() => {
+    mock.reset()
+  });
+
+  it('creates the right actions for signedTransaction', async () => {
+    const dispatches = await Thunk(actions.signedTransaction).execute();
+
+    expect(dispatches.length).toBe(2);
+
+  });
+
+  it('creates the right actions for broadcastTransaction', async () => {
+    const dispatches = await Thunk(actions.broadcastTransaction).execute();
+
+    expect(dispatches.length).toBe(2);
+  });
 })
--- src/__tests__/actions/transactions.test.js
+++ src/__tests__/actions/transactions.test.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import { mount } from 'enzyme';
+import { Thunk } from 'redux-testkit';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import MockAdapter from 'axios-mock-adapter';
 
-import Header from 'components/header';
+import { client } from 'util/axios';
+import * as actions from 'actions';
+import * as types from 'actions/types';
 
-describe('Transactions', () => {
-  it('placeholder test', () => {
-    expect(2 + 2).toBe(4);
-  })
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const mock = new MockAdapter(client);
+
+describe('Transactions actions', () => {
+  afterEach(() => {
+    mock.reset()
+  });
+
+  it('creates the right actions for pendingTransactions', async () => {
+    const dispatches = await Thunk(actions.pendingTransactions).execute();
+
+    expect(dispatches.length).toBe(2);
+
+  });
+
+  it('creates the right actions for transactionDetail', async () => {
+    const dispatches = await Thunk(actions.transactionDetail).execute();
+
+    expect(dispatches.length).toBe(2);
+  });
+
+  it('creates the right actions for transactionExecute', async () => {
+    const dispatches = await Thunk(actions.transactionExecute).execute();
+
+    expect(dispatches.length).toBe(2);
+  });
+
+  it('creates the right actions for initializeTransaction', async () => {
+    const dispatches = await Thunk(actions.initializeTransaction).execute();
+
+    expect(dispatches.length).toBe(2);
+  });
 })
