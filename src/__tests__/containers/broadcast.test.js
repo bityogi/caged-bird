0c09e1772a699f1c91104a86e52316058be60037
bityogi <bityogi@tutanota.com>
2018-03-12 20:18:11 -0400

    tests -- containers

---
+++ src/__tests__/containers/broadcast.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { MemoryRouter } from 'react-router';
+
+import Broadcast from 'containers/transactions';
+import store from 'store';
+
+describe('Login', () => {
+  it('renders correctly', () => {
+    const rendered = renderer.create(
+      <Provider store={store}>
+        <MuiThemeProvider>
+          <MemoryRouter initialEntries={['/', '/landing']}>
+            <Broadcast />
+          </MemoryRouter>
+        </MuiThemeProvider>
+      </Provider>
+
+    );
+    expect(rendered.toJSON()).toMatchSnapshot();
+  });
+
+});
