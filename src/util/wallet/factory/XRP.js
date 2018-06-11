6d66d27e5ce20c7c1f56bd256627a5eeb6a97c4e
bityogi <bityogi@tutanota.com>
2018-06-11 14:51:52 -0400

    ripple decoding tx -- stable

--- src/util/wallet/factory/XRP.js
+++ src/util/wallet/factory/XRP.js
@@ -9,9 +9,10 @@ export default class XRP extends Wallet {
         let deferred = Q.defer();
         
         try {
-            // const rawHex = hex.substring(64);
-            // const tx = decode(rawHex);
-            deferred.resolve();
+            const rawHex = hex.substring(64);
+            const tx = decode(rawHex);
+            console.log('decoded ripple tx: ', tx);
+            deferred.resolve(tx);
         } catch (error) {
             console.error('Error decoding tx: ', error);
             deferred.reject({ message: 'Error decoding transaction', error: error });
