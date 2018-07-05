b483f9fd9e82f794ae79b930fc3c2fd9832bb82e
bityogi <bityogi@tutanota.com>
2018-07-05 16:00:20 -0400

    jira 233 -- add json view of decoded tx -- XRP variants

--- src/containers/broadcast/decodedTransaction/XRP.js
+++ src/containers/broadcast/decodedTransaction/XRP.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { List, ListItem } from 'material-ui/List';
 import ActionInfo from 'material-ui/svg-icons/action/info';
 import FontIcon from 'material-ui/FontIcon';
+import Divider from 'material-ui/Divider';
+import ReactJson from 'react-json-view';
 
 import theme from 'theme';
 const { palette: { primary1Color } } = theme;
@@ -26,37 +28,42 @@ class XRP extends Component {
         console.log('decodedTX (XRP): ', decodedTx);
         
         return(
-            <List>
-                <ListItem
-                    primaryText="Destination"
-                    secondaryText={decodedTx.Destination}
-                    innerDivStyle={styles.listItems}
-                    leftIcon={<ActionInfo style={styles.icon} color={primary1Color} />}
-                />
-                
-                <ListItem
-                    primaryText="Amount"
-                    secondaryText={decodedTx.Amount}
-                    innerDivStyle={styles.listItems}
-                    leftIcon={<FontIcon className="material-icons" style={styles.icon}>person_pin</FontIcon>}
-                />
+            <div>
+                 <List>
+                    <ListItem
+                        primaryText="Destination"
+                        secondaryText={decodedTx.Destination}
+                        innerDivStyle={styles.listItems}
+                        leftIcon={<ActionInfo style={styles.icon} color={primary1Color} />}
+                    />
+                    
+                    <ListItem
+                        primaryText="Amount"
+                        secondaryText={decodedTx.Amount}
+                        innerDivStyle={styles.listItems}
+                        leftIcon={<FontIcon className="material-icons" style={styles.icon}>person_pin</FontIcon>}
+                    />
 
-                <ListItem
-                    primaryText="Account"
-                    secondaryText={decodedTx.Account}
-                    innerDivStyle={styles.listItems}
-                    leftIcon={<FontIcon className="material-icons" style={styles.icon}>album</FontIcon>}
-                />
+                    <ListItem
+                        primaryText="Account"
+                        secondaryText={decodedTx.Account}
+                        innerDivStyle={styles.listItems}
+                        leftIcon={<FontIcon className="material-icons" style={styles.icon}>album</FontIcon>}
+                    />
 
-                <ListItem
-                    primaryText="Fee"
-                    secondaryText={decodedTx.Fee}
-                    innerDivStyle={styles.listItems}
-                    leftIcon={<FontIcon className="material-icons" style={styles.icon}>account_balance</FontIcon>}
-                />
-
-
-            </List>
+                    <ListItem
+                        primaryText="Fee"
+                        secondaryText={decodedTx.Fee}
+                        innerDivStyle={styles.listItems}
+                        leftIcon={<FontIcon className="material-icons" style={styles.icon}>account_balance</FontIcon>}
+                    />
+                </List>
+                <Divider />
+                <div>
+                    <ReactJson src={decodedTx} />
+                </div>
+            </div>
+           
         )
     }
 }
