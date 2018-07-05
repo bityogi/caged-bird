a3c481bec41bc4f5f7f451b39deec9c6a9c76a63
bityogi <bityogi@tutanota.com>
2018-07-05 15:41:25 -0400

    jira 233 -- decode tx details for BTC variants

--- src/containers/broadcast/decodedTransaction/BTC.js
+++ src/containers/broadcast/decodedTransaction/BTC.js
@@ -1,13 +1,72 @@
 import React, { Component } from 'react';
 import ReactJson from 'react-json-view';
+import bitcoin from 'bitcoinjs-lib';
+import _ from 'lodash';
+import { List, ListItem } from 'material-ui/List';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+import Divider from 'material-ui/Divider';
+
+import theme from 'theme';
+
+const { palette: { primary1Color } } = theme;
+
+const styles = {
+  icon: {
+    color: primary1Color,
+  },
+  listItems : {
+    paddingTop: 10,
+    paddingLeft: 50,
+    paddingBottom: 2,
+  },
+  error: {
+    color: '#FF0000'
+  }
+}
 
 class BTC extends Component {
+
+    renderDestinationAddress(tx) {
+
+        const addresses = _.map(tx.outs, (out, index) => {
+            const pubKey = Buffer.from(out.script, 3, 20);
+            const address = bitcoin.address.fromOutputScript(pubKey);
+            console.log('btc to address: ', address);
+            return (
+                <ListItem
+                    key={index}
+                    primaryText={`To :${address}`}
+                    secondaryText={out.value}
+                    innerDivStyle={styles.listItems}
+                    leftIcon={<ActionInfo style={styles.icon} color={primary1Color} />}
+                />
+            )
+            
+        })
+
+        console.log('address items: ', addresses);
+        return addresses;
+    }
+
     render() {
         const { decodedTx } = this.props;
         console.log('decodedTX (BTC) : ', decodedTx);
         
         return(
-            <ReactJson src={decodedTx} />
+            <div>
+                <div>
+                    <List>
+                        {this.renderDestinationAddress(decodedTx)}
+                    </List>
+                </div>
+                <Divider />
+                <div>
+                    <ReactJson src={decodedTx} />
+                </div>
+
+
+            </div>
+           
         )
     }
 }
